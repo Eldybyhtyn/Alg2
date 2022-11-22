57513f8b707e7fe2de6586c4b823c0fe6ca71405
Eldybyhtyn <name66437@gmail.com>
2022-11-22 16:10:06 +0300

    comp

---
+++ Graph.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace Lol {
+
+    public class Graph {
+        int countOfArsc;
+        List<List<int>> table = new List<List<int>>();
+        List<string> names = new List<string>();
+        
+        public Graph() {
+            countOfArsc = 0;
+        }
+
+        public void Add_V(string name) {
+            table.Add(new List<int>());
+            names.Add(name);
+        }
+
+        public void Add_E(int first, int second, int cost) {
+            ++countOfArsc;
+            table[first - 1].Add(cost);
+            table[second - 1].Add(-cost);
+            for (int i = 0; i < table.Count; ++i) {
+                if (table[i].Count == countOfArsc) continue;
+                else table[i].Add(0);
+            }
+        }
+
+        public void Del_V(string name) {
+            int index = names.IndexOf(name);
+            table.RemoveAt(index);
+        }
+
+        public void Del_E(int first, int second) {
+            //table[first - 1][]
+        }
+
+        public void Edit_E(int first, int second, int cost) {
+            //table[first - 1][]
+        }
+
+        public void ShowGraph() {
+            for (int i = 0; i < table.Count; ++i) {
+                for (int j = 0; j < table[i].Count; ++j) {
+                    Console.Write(Convert.ToString(table[i][j]) + " ");
+                }
+            
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lol {
+
+    class Program {
+
+        static void Main() {
+
+            Graph hi = new Graph();
+            hi.Add_V("first");
+            hi.Add_V("second");
+            hi.Add_V("third");
+            hi.Add_E(1, 2, 1);
+            hi.Add_E(1, 3, 4);
+            hi.Add_E(2, 1, 8);
+            
+            hi.ShowGraph();
+            System.Console.WriteLine();
+            hi.Del_E(1, 2);
+            hi.Del_V("first");
+            hi.ShowGraph();
+        }
+
+
+    }
+
+
+
+}
